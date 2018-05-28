c7b010165b97a10b67a7a9a61382158ba4171d60
tef <tef@twentygototen.org>
2018-05-28 03:45:47 +0000

    Skeleton of fetch/serve

--- kson.py
+++ kson.py
@@ -1,10 +1,16 @@
 """
     kson rpc: json rpc with kubernetes like objects
 
-
 """
 
 import json
+import threading
+import socket
+import traceback
+import sys
+import urllib.request
+
+from wsgiref.simple_server import make_server, WSGIRequestHandler
 
 class Registry:
     KIND = 'kind'
@@ -27,8 +33,8 @@ class Registry:
             return fn
         return _decorate
 
-    def parse(self, text):
-        document = json.loads(text)
+    def parse(self, buf):
+        document = json.loads(buf.decode('utf-8'))
         kind, apiVersion = document.pop(self.KIND, None), document.pop(self.API_VERSION, None)
         if kind is None or apiVersion is None:
             raise Exception('Invalid JSON document: missing {}, {}'.format(self.KIND, self.API_VERSION))
@@ -44,23 +50,143 @@ class Registry:
         for k,v in obj.__dict__.items():
             if not k.startswith('_') and not k in (self.KIND, self.API_VERSION,):
                 document[k] = v
-        return json.dumps(document)
+        return json.dumps(document).encode('utf-8')
 
 registry = Registry()
+
+class wire:
+    @registry.add('Request', 'kson/v1')
+    class Request:
+        def __init__(self, metadata, state, attributes):
+            self.metadata = metadata
+            self.state = state
+            self.attributes = attributes
+
+    @registry.add('Response', 'kson/v1')
+    class Response:
+        def __init__(self, metadata, state, attributes):
+            self.metadata = metadata
+            self.state = state
+            self.attributes = attributes
+
+    @registry.add('Service', 'kson/v1')
+    class Service:
+        def __init__(self, metadata, state, attributes):
+            self.metadata = metadata
+            self.state = state
+            self.attributes = attributes
+
+    @registry.add('Collection', 'kson/v1')
+    class Collection:
+        def __init__(self, metadata, state, attributes):
+            self.metadata = metadata
+            self.state = state
+            self.attributes = attributes
+    
+    @registry.add('Cursor', 'kson/v1')
+    class Cursor:
+        def __init__(self, metadata, state, attributes):
+            self.metadata = metadata
+            self.state = state
+            self.attributes = attributes
+    @registry.add('Future', 'kson/v1')
+    class Future: 
+        def __init__(self, metadata, state, attributes):
+            self.metadata = metadata
+            self.state = state
+            self.attributes = attributes
+
+class server:
+    class RequestHandler(WSGIRequestHandler):
+        def log_request(self, code='-', size='-'):
+            pass
+
+    class Server(threading.Thread):
+        def __init__(self, app, host="", port=0, request_handler=None):
+            if request_handler is None:
+                request_handler = server.RequestHandler
+            threading.Thread.__init__(self)
+            self.daemon=True
+            self.running = True
+            self.server = make_server(host, port, app,
+                handler_class=request_handler)
+
+        @property
+        def url(self):
+            return u'http://%s:%d/'%(self.server.server_name, self.server.server_port)
+
+        def run(self):
+            self.running = True
+            while self.running:
+                self.server.handle_request()
+
+        def stop(self):
+            self.running =False
+            if self.server and self.is_alive():
+                try:
+                    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+                    s.connect(self.server.socket.getsockname()[:2])
+                    s.send(b'\r\n')
+                    s.close()
+                except IOError:
+                    import traceback
+                    traceback.print_exc()
+            self.join(5)
+
+class Endpoint:
+    def __init__(self, prefix='/', version='v1'):
+        self.prefix = prefix
+        self.version = version
+
+    def __call__(self, environ, start_response):
+        start_response('200 Ok', [])
+        return [dump(wire.Response({},{}, {}))]
+
 parse, dump = registry.parse, registry.dump
 
+def serve(endpoint):
+    return server.Server(app=endpoint, port=1729)
 
-@registry.add('Request', 'kson/v1')
-class Request:
-    def __init__(self, metadata, state, attributes):
-        self.metadata = metadata
-        self.state = state
-        self.attributes = attributes
+def fetch(url):
+    if isinstance(url, str):
+        request =  urllib.request.Request(url)
+    elif isinstance(url, wire.Request):
+        pass
 
-@registry.add('Response', 'kson/v1')
-class Response:
-    def __init__(self, metadata, state, attributes):
-        self.metadata = metadata
-        self.state = state
-        self.attributes = attributes
+    with urllib.request.urlopen(request) as response:
+        data = response.read()
+        obj = parse(data)
+
+    if isinstance(obj, wire.Response):
+        pass
+    elif isinstance(obj, wire.Collection):
+        pass
+    elif isinstance(obj, wire.Cursor):
+        pass
+
+
+    else:
+        pass
+    return obj
+    pass
+
+if __name__ == '__main__':
+
+    class MyEndpoint(Endpoint):
+
+        def rpc_one(self, args):
+            return {'args': args}
+
+    thread = serve(MyEndpoint())
+    thread.start()
+
+    url = thread.url
+
+    service = fetch(url)
+
+    # response = fetch(service.rpc_one())
+
+    # print(response)
+
+    
 
